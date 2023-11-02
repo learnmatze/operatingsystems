c2eb9e8ded35b26d4ca26e48b9d885594f12caf7
learnmatze <maschober@web.de>
2023-11-02 13:53:04 +0100

    adding race condition with and without lock

---
+++ multithreading/race_condition_with_lock.py
@@ -0,0 +1,49 @@
+import threading
+import time
+
+def adder(amount, repeats):
+    global value
+    for _ in range(repeats):
+        with lock:
+            # copy the value
+            tmp = value
+            # suggest a context switch
+            time.sleep(0)
+            # change the copy
+            tmp = tmp + amount
+            # suggest a context switch
+            time.sleep(0)
+            # copy the value back
+            value = tmp
+ 
+# make subtractions from the global variable
+def subtractor(amount, repeats):
+    global value
+    for _ in range(repeats):
+        with lock:
+            # copy the value
+            tmp = value
+            # suggest a context switch
+            time.sleep(0)
+            # change the copy
+            tmp = tmp - amount
+            # suggest a context switch
+            time.sleep(0)
+            # copy the value back
+            value = tmp
+   
+# define the global variable
+value = 0
+lock = threading.Lock()
+# start a thread making additions
+adder_thread = threading.Thread(target=adder, args=(1, 1000))
+# start a thread making subtractions
+subtractor_thread = threading.Thread(target=subtractor, args=(1, 1000))
+adder_thread.start()
+subtractor_thread.start()
+# wait for both threads to finish
+print('Waiting for threads to finish...')
+adder_thread.join()
+subtractor_thread.join()
+# report the value
+print(f'Value: {value}')
